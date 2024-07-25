2b5222e07a90794da579db43baeb506520ef6cbd
Chris Esplin <chris@chrisesplin.com>
2024-07-25 10:27:45 -0600

    Outstatic install

---
+++ src/app/(cms)/layout.tsx
@@ -0,0 +1,11 @@
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
